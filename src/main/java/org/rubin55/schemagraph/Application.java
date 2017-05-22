792181d47ce91a53e650d9f08aff1a2fec8c50bf
Rubin Simons <rubin@raaftech.com>
2017-05-22 20:21:13 +0200

    Added pseudo-code comment to sketch gist

--- src/main/java/org/rubin55/schemagraph/Application.java
+++ src/main/java/org/rubin55/schemagraph/Application.java
@@ -3,8 +3,6 @@ package org.rubin55.schemagraph;
 import org.rubin55.schemagraph.oracle.Table;
 import org.rubin55.schemagraph.oracle.Schema;
 
-import ch.qos.logback.classic.Level;
-
 import org.neo4j.ogm.session.Session;
 import org.neo4j.ogm.session.SessionFactory;
 
@@ -16,7 +14,6 @@ public class Application {
 
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
-
     public static void main(String[] args) {
         log.info("Let's do some stuff.");
 
@@ -27,15 +24,27 @@ public class Application {
         final SessionFactory sessionFactory = new SessionFactory("org.rubin55.schemagraph");
         Session session = sessionFactory.openSession();
 
-        Schema schema = new Schema("Rubin");
+        Schema schema1 = new Schema("Rubin");
+        Schema schema2 = new Schema("Rubin");
 
         Table table1 = new Table("SomeTable");
         Table table2 = new Table("OtherTable");
 
-        schema.takeOwnership(table1);
-        schema.takeOwnership(table2);
+        schema1.takeOwnership(table1);
+        schema2.takeOwnership(table2);
         table1.addReference(table2);
 
-        session.save(schema);
+        session.save(schema1);
+        session.save(schema2);
+
+        // {"OWNER": "RUBIN", "NAME": "SomeTable", "TYPE": "TABLE", "REFERENCED_OWNER": "RUBIN", "REFERENCED_NAME": "OtherTable", "REFERENCED_TYPE": "TABLE"},
+        //owner, referenced_owner : An Entity schema subtype
+        //type, referenced_type : An Item subtype
+        //name, referenced_name : name of item
+        // for object in array do
+        //   schema1 = new Schema(object.OWNER), findClassByType(object.type) -> item1 = new SubItem(object.NAME), schema1.takeOwnership(item1)
+        //   schema2 = new Schema(object.REFERENCED_OWNER), findClassByType(object.type) -> item2 = new SubItem(object.REFERENCED_NAME), schema2.takeOwnership(item2)
+        //   item1.addReference(item2)
+        // done
     }
 }
